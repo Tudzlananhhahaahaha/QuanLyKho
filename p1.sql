d8d9690237b3519357c0590ef4611766685f63b5
tuandungx <dangtuandung1909206@gmail.com>
2025-11-11 09:49:59 +0700

    add file sql

---
+++ p1.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Kho;
+SELECT * FROM NhaCungCap;
+SELECT * FROM MatHang;
+SELECT * FROM PhieuNhap;
+SELECT * FROM ChiTietPhieuNhap;
+SELECT * FROM PhieuXuat;
+SELECT * FROM ChiTietPhieuXuat;
