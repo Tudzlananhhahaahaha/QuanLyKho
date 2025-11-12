9a2ee12b0696e1288d6a2a11ab1adfaf644d85c0
tuandungx <dangtuandung1909206@gmail.com>
2025-11-12 11:07:15 +0700

    Delete all SELECT queries from p1.sql
    Removed all SELECT statements from p1.sql.

--- p1.sql
+++ p1.sql
@@ -1,7 +1,1 @@
-SELECT * FROM Kho;
-SELECT * FROM NhaCungCap;
-SELECT * FROM MatHang;
-SELECT * FROM PhieuNhap;
-SELECT * FROM ChiTietPhieuNhap;
-SELECT * FROM PhieuXuat;
-SELECT * FROM ChiTietPhieuXuat;
+
